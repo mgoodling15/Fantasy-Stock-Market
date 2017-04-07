84444e404ae35b740899c7ec9e614dcc76a0d1eb
mgoodling15 <megoodling@davidson.edu>
2017-04-07 13:00:47 -0400

    Add files via upload

---
+++ angular2/fsm-directory/src/app/login-form/login-form.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+import { Player }    from './player';
+
+@Component({
+  selector: 'login-form',
+  templateUrl: './login-form.component.html'
+})
+export class LoginFormComponent {
+
+  model = new Player(0, "Email", "Username", "Password", "Bio", 100000);
+
+  submitted = false;
+
+  onSubmit() { this.submitted = true; }
+
+  // TODO: Remove this when we're done
+  get diagnostic() { return JSON.stringify(this.model); }
+}
---
+++ angular2/fsm-directory/src/app/player-form/player-form.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+import { Player }    from './player';
+
+@Component({
+  selector: 'player-form',
+  templateUrl: './player-form.component.html'
+})
+export class PlayerFormComponent {
+
+  model = new Player(0, "Email", "Username", "Password", "Bio", 100000);
+
+  submitted = false;
+
+  onSubmit() { this.submitted = true; }
+
+  // TODO: Remove this when we're done
+  get diagnostic() { return JSON.stringify(this.model); }
+}
