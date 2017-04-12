3c74ab742a2bcceb6420a9ee3d0eacd461bb3130
mgoodling15 <megoodling@davidson.edu>
2017-04-12 09:32:18 -0400

    Add files via upload

---
+++ league-view/league-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-directory',
+  templateUrl: './league-view.component.html',
+  styleUrls: ['./league-view.component.css']
+})
+export class LeagueViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
