d1831a17a21cfefd0b02728855010c3dcfe2f51a
mgoodling15 <megoodling@davidson.edu>
2017-04-14 19:48:58 -0400

    Add files via upload

---
+++ angular2/fsm-directory/src/app/buy-sell-view/buy-sell-view.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-buy-sell-view',
+  templateUrl: './buy-sell-view.component.html',
+  styleUrls: ['./buy-sell-view.component.css']
+})
+export class BuySellViewComponent implements OnInit {
+
+  stocks = [
+    {
+      "name": "Amazon",
+      "value": 123456
+    },
+    {
+      "name": "Microsoft",
+      "value": 747383
+    },
+    {
+      "name": "Exxon",
+      "value": 970442
+    },
+    {
+      "name": "Panera",
+      "value": 2554373
+    }
+  ]
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
