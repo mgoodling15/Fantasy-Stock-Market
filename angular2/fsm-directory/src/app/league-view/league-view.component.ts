fdd03774f08a35b13a76e58864989af7ffdee9b7
mgoodling15 <megoodling@davidson.edu>
2017-04-16 11:26:42 -0400

    Update league-view.component.ts

--- angular2/fsm-directory/src/app/league-view/league-view.component.ts
+++ angular2/fsm-directory/src/app/league-view/league-view.component.ts
@@ -1,13 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
-  selector: 'app-directory',
+  selector: 'app-league-view',
   templateUrl: './league-view.component.html',
   styleUrls: ['./league-view.component.css']
 })
 export class LeagueViewComponent implements OnInit {
+  league: string;
+  player: string;
 
-  constructor() { }
+  players = [
+    {"username": "Megan", "password": "asdfasdf"},
+    {"username": "Riley", "password": "qwerqwer"},
+    {"username": "Ripple", "password": "nbnbnbn"},
+  ]
+
+  constructor(private route: ActivatedRoute) {
+    this.league = route.snapshot.params['league'];
+   }
 
   ngOnInit() {
   }
