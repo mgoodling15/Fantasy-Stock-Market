363946350f8c3558fadfc2747d4bc0c9e5ab5f68
mgoodling15 <megoodling@davidson.edu>
2017-04-16 11:28:30 -0400

    Update player-view.component.ts

--- angular2/fsm-directory/src/app/player-view/player-view.component.ts
+++ angular2/fsm-directory/src/app/player-view/player-view.component.ts
@@ -1,26 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-player-view',
   templateUrl: './player-view.component.html',
-  styleUrls: ['./player-view.component.css']
+  styleUrls: ['./player-view.component.css'],
+  providers: [DataService]
 })
 export class PlayerViewComponent implements OnInit {
   classes = {'blue': false, 'red': true, 'underline': false};
   player: string;
 
-  constructor(private route: ActivatedRoute) {
+  constructor(private route: ActivatedRoute, private dataService: DataService) {
     this.player = route.snapshot.params['player'];
    }
 
-   players = [
-     {"username": "Megan", "password": "asdfasdf"},
-     {"username": "Riley", "password": "qwerqwer"},
-     {"username": "Ripple", "password": "nbnbnbn"},
-   ]
+   players = []
 
   ngOnInit() {
+
   }
 
 }
