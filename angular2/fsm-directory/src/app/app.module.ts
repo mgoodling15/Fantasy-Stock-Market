6bd90189198d2df253caab47777a3177e4a70b00
mgoodling15 <megoodling@davidson.edu>
2017-04-16 11:29:34 -0400

    Update app.module.ts

--- angular2/fsm-directory/src/app/app.module.ts
+++ angular2/fsm-directory/src/app/app.module.ts
@@ -2,21 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { DirectoryComponent } from './directory/directory.component';
+import { LeagueViewComponent } from './league-view/league-view.component';
+import { PlayerFormComponent } from './player-form/player-form.component';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { PlayerViewComponent } from './player-view/player-view.component';
+import { Routing } from './app.routes';
+import { BuySellViewComponent } from './buy-sell-view/buy-sell-view.component';
+import { FilterPipe } from './filter.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    DirectoryComponent
+    LeagueViewComponent,
+    PlayerFormComponent,
+    LoginFormComponent,
+    PlayerViewComponent,
+    BuySellViewComponent,
+    FilterPipe,
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    Routing
   ],
   providers: [],
   bootstrap: [AppComponent]
