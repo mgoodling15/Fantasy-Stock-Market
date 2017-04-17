e27f7771f63daeb9299d43b8891ec53e6825f463
mgoodling15 <megoodling@davidson.edu>
2017-04-16 20:32:50 -0400

    Update player-form.component.ts

--- angular2/fsm-directory/src/app/player-form/player-form.component.ts
+++ angular2/fsm-directory/src/app/player-form/player-form.component.ts
@@ -1,17 +1,35 @@
-import { Component } from '@angular/core';
-import { Player }    from './player';
+import { Component, OnInit } from '@angular/core';
+import { Player }    from '../player';
 import { DataService } from '../data.service';
 
 @Component({
   selector: 'player-form',
-  templateUrl: './player-form.component.html'
+  templateUrl: './player-form.component.html',
+  providers: [DataService]
 })
-export class PlayerFormComponent {
 
-  model = new Player(0, "Email", "Username", "Password", "Bio", 100000);
-
+export class PlayerFormComponent implements OnInit {
+  errorMessage: string;
+  players: Player[];
+  mode = 'Observable';
   submitted = false;
+  model = new Player("Email", "Username", "Password", "Bio");
 
+  constructor (private dataService: DataService) {}
+  ngOnInit() {}
+  // getPlayers() {
+  //   this.dataService.getPlayers()
+  //                    .subscribe(
+  //                      players => this.players = players,
+  //                      error =>  this.errorMessage = <any>error);
+  // }
+  // addPlayer(name: string) {
+  //   if (!name) { return; }
+  //   this.dataService.addPlayer(name)
+  //                    .subscribe(
+  //                      hero  => this.players.push(player),
+  //                      error =>  this.errorMessage = <any>error);
+  // }
   onSubmit() {
 
     this.submitted = true;
@@ -19,6 +37,4 @@ export class PlayerFormComponent {
 
   }
 
-  // TODO: Remove this when we're done
-  get diagnostic() { return JSON.stringify(this.model); }
 }
