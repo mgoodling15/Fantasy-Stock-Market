673f3cf3b17ef070b67fdb825ff380fdb205af25
mgoodling15 <megoodling@davidson.edu>
2017-04-16 20:31:41 -0400

    Update league-view.component.ts

--- angular2/fsm-directory/src/app/league-view/league-view.component.ts
+++ angular2/fsm-directory/src/app/league-view/league-view.component.ts
@@ -1,26 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { DataService } from '../data.service';
+declare var firebase: any;
 
 @Component({
   selector: 'app-league-view',
   templateUrl: './league-view.component.html',
-  styleUrls: ['./league-view.component.css']
+  styleUrls: ['./league-view.component.css'],
+  providers: [DataService]
 })
 export class LeagueViewComponent implements OnInit {
   league: string;
   player: string;
 
-  players = [
-    {"username": "Megan", "password": "asdfasdf"},
-    {"username": "Riley", "password": "qwerqwer"},
-    {"username": "Ripple", "password": "nbnbnbn"},
-  ]
+  players = []
 
-  constructor(private route: ActivatedRoute) {
+  constructor(private route: ActivatedRoute, private dataService: DataService) {
     this.league = route.snapshot.params['league'];
    }
 
-  ngOnInit() {
-  }
+   ngOnInit() {
+      this.fbGetData();
+   }
 
+   fbGetData() {
+    firebase.database().ref('/players').on('child_added', (snapshot) => {
+      this.players.push(snapshot.val())
+    })
+  }
 }
