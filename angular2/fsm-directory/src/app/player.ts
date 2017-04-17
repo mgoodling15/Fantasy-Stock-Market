2b198dbc127bb076090d0aed95e28de356a8ec7a
mgoodling15 <megoodling@davidson.edu>
2017-04-17 11:05:24 -0400

    Update player.ts

--- angular2/fsm-directory/src/app/player.ts
+++ angular2/fsm-directory/src/app/player.ts
@@ -1,11 +1,8 @@
 export class Player {
   constructor(
-    public id: number,
     public email: string,
     public username: string,
     public password: string,
-    public bio: string,
-    public portfolio: Number
-    //add picture, bio, email
+    public bio: string
   ) {  }
 }
