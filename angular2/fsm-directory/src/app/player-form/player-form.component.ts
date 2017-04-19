cbcaf4278f534509108546f2614cd2e0580193fb
mgoodling15 <megoodling@davidson.edu>
2017-04-19 13:16:45 -0400

    Update player-form.component.ts

--- angular2/fsm-directory/src/app/player-form/player-form.component.ts
+++ angular2/fsm-directory/src/app/player-form/player-form.component.ts
@@ -1,45 +1,85 @@
-import { Component, OnInit } from '@angular/core';
-import { Player }    from '../player';
-import { DataService } from '../data.service';
-declare var firebase: any;
 
-@Component({
-  selector: 'player-form',
-  templateUrl: './player-form.component.html',
-  providers: [DataService]
-})
+<!-- <div> {{diagnostic}} </div> -->
+<div id = "add-player"[hidden]="submitted">
+  <div class="container">
+      <h1>Create Account</h1>
+      <form (ngSubmit)="onSubmit(model.email);fbPostPlayer(model.email,
+      model.username, model.password, model.bio)" #playerForm="ngForm">
+      <div class="form-group">
+        <label for="email">Email</label>
+        <input type="text" class="form-control" id="email"
+        required
+        [(ngModel)]="model.email" name="email"
+        #name="ngModel">
+        <div [hidden]="name.valid || name.pristine"
+        class="alert alert-danger">
+        Email is required
+        </div>
+      </div>
 
-export class PlayerFormComponent implements OnInit {
-  errorMessage: string;
-  players: Player[];
-  mode = 'Observable';
-  submitted = false;
-  model = new Player("Email", "Username", "Password", "Bio");
+      <div class="form-group">
+        <label for="username">Username</label>
+        <input type="text" class="form-control" id="username"
+        required
+        [(ngModel)]="model.username" name="username"
+        #name="ngModel">
+        <div [hidden]="name.valid || name.pristine"
+        class="alert alert-danger">
+        Username is required
+        </div>
+      </div>
 
-  email: "";
-  username: "";
-  password: "";
-  bio: ""
+      <div class="form-group">
+        <label for="password">Password</label>
+        <input type="text" class="form-control" id="password"
+        required
+        [(ngModel)]="model.password" name="password"
+        #name="ngModel">
+        <div [hidden]="name.valid || name.pristine"
+        class="alert alert-danger">
+        Password is required
+        </div>
+      </div>
 
-  constructor (private dataService: DataService) {}
-  ngOnInit() { }
-  // getPlayers() {
-  //   this.dataService.getPlayers()
-  //                    .subscribe(
-  //                      players => this.players = players,
-  //                      error =>  this.errorMessage = <any>error);
-  // }
-  // addPlayer(name: string) {
-  //   if (!name) { return; }
-  //   this.dataService.addPlayer(name)
-  //                    .subscribe(
-  //                      hero  => this.players.push(player),
-  //                      error =>  this.errorMessage = <any>error);
-  // }
-  fbPostPlayer(email, username, password, bio) {
-    this.submitted = true;
-    var newPlayer = new Player(email, username, password, bio);
-    firebase.database().ref('/players').push({username: {email: email, username: username,
-      password: password, bio: bio}});
-  }
-}
+    <div class="form-group">
+      <label for="bio">Bio</label>
+      <input type="text" class="form-control" id="bio"
+      required
+      [(ngModel)]="model.bio" name="bio"
+      #name="ngModel">
+      <div [hidden]="name.valid || name.pristine"
+      class="alert alert-danger">
+      Bio is required
+      </div>
+    </div>
+
+    <button type="submit" class="btn btn-success"
+    [disabled]="!playerForm.form.valid">Submit</button>
+    </form>
+  </div>
+</div>
+
+<div [hidden]="!submitted">
+  <h2>You submitted the following:</h2>
+  <div class="row">
+    <div class="col-xs-3">Email</div>
+    <div class="col-xs-9 pull-left">{{ model.email }}</div>
+  </div>
+
+  <div class="row">
+    <div class="col-xs-3">Username</div>
+    <div class="col-xs-9  pull-left">{{ model.username }}</div>
+  </div>
+  <div class="row">
+    <div class="col-xs-3">Password</div>
+    <div class="col-xs-9 pull-left">{{ model.password }}</div>
+  </div>
+  <div class="row">
+    <div class="col-xs-3">Bio</div>
+    <div class="col-xs-9 pull-left">{{ model.bio }}</div>
+  </div>
+
+  <br>
+  <button class="btn btn-primary" (click)="submitted=false">Edit</button>
+</div>
+<div>
