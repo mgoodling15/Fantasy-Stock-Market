8c692008487ce1c7ab0cfaca8c1eaed95d723574
alherronWC <alexanderfherron@gmail.com>
2017-04-19 14:26:58 -0400

    Add files via upload

--- express/routes/api.js
+++ express/routes/api.js
@@ -1,39 +1,68 @@
-    const express = require('express');
-    const router = express.Router();
-    const Player = require('../models/player');
-    const League = require('../models/league');
-   
+
+var stockSymbols = ['ABT', 'ABBV', 'ACN', 'ACE', 'ADBE', 'ADT', 'AAP', 'AES', 'AET', 'AFL', 'AMG', 'A', 'GAS', 'APD', 'ARG', 'AKAM', 'AA', 'AGN', 'ALXN', 'ALLE', 'ADS',
+		    'ALL', 'AMZN', 'AEE', 'AAL', 'AEP', 'AXP', 'AIG', 'AMT', 'AMP', 'ABC', 'AME', 'AMGN', 'APH', 'APC', 'ADI', 'AON', 'APA', 'AIV', 'AMAT',
+		    'ADM', 'AIZ', 'T', 'ADSK', 'ADP', 'AN', 'AZO', 'AVGO', 'AVB', 'AVY', 'BHI', 'BLL', 'BAC', 'BK', 'BCR', 'BXLT', 'BAX', 'BBT', 'BDX', 'BBBY', 'BRK-B',
+		    'BBY', 'BLX', 'HRB', 'BA', 'BWA', 'BXP', 'BSK', 'BMY', 'BRCM', 'BF-B', 'CHRW', 'CA', 'CVC', 'COG', 'CAM', 'CPB', 'COF', 'CAH', 'HSIC', 'KMX', 'CCL',
+		    'CAT', 'CBG', 'CBS', 'CELG', 'CNP', 'CTL', 'CERN', 'CF', 'SCHW', 'CHK', 'CVX', 'CMG', 'CB', 'CI', 'XEC', 'CINF', 'CTAS', 'CSCO', 'C', 'CTXS', 'CLX',
+		    'CME', 'CMS', 'COH', 'KO', 'CCE', 'CTSH', 'CL', 'CMCSA', 'CMA', 'CSC', 'CAG', 'COP', 'CNX', 'ED', 'STZ', 'GLW', 'COST', 'CCI', 'CSX', 'CMI', 'CVS',
+		    'DHI', 'DHR', 'DRI', 'DVA', 'DE', 'DLPH', 'DAL', 'XRAY', 'DVN', 'DO', 'DTV', 'DFS', 'DISCA', 'DISCK', 'DG', 'DLTR', 'D', 'DOV', 'DOW', 'DPS', 'DTE',
+		    'DD', 'DUK', 'DNB', 'ETFC', 'EMN', 'ETN', 'EBAY', 'ECL', 'EIX', 'EW', 'EA', 'EMC', 'EMR', 'ENDP', 'ESV', 'ETR', 'EOG', 'EQT', 'EFX', 'EQIX', 'EQR',
+		    'ESS', 'EL', 'ES', 'EXC', 'EXPE', 'EXPD', 'ESRX', 'XOM', 'FFIV', 'FB', 'FAST', 'FDX', 'FIS', 'FITB', 'FSLR', 'FE', 'FSIV', 'FLIR', 'FLS', 'FLR',
+		    'FMC', 'FTI', 'F', 'FOSL', 'BEN', 'FCX', 'FTR', 'GME', 'GPS', 'GRMN', 'GD', 'GE', 'GGP', 'GIS', 'GM', 'GPC', 'GNW', 'GILD', 'GS', 'GT', 'GOOGL',
+		    'GOOG', 'GWW', 'HAL', 'HBI', 'HOG', 'HAR', 'HRS', 'HIG', 'HAS', 'HCA', 'HCP', 'HCN', 'HP', 'HES', 'HPQ', 'HD', 'HON', 'HRL', 'HSP', 'HST', 'HCBK',
+		    'HUM', 'HBAN', 'ITW', 'IR', 'INTC', 'ICE', 'IBM', 'IP', 'IPG', 'IFF', 'INTU', 'ISRG', 'IVZ', 'IRM', 'JEC', 'JBHT', 'JNJ', 'JCI', 'JOY', 'JPM',
+		    'JNPR', 'KSU', 'K', 'KEY', 'GMCR', 'KMB', 'KIM', 'KMI', 'KLAC', 'KSS', 'KRFT', 'KR', 'LB', 'LLL', 'LH', 'LRCX', 'LM', 'LEG', 'LEN', 'LVLT', 'LUK',
+		    'LLY', 'LNC', 'LLTC', 'LMT', 'L', 'LOW', 'LYB', 'MTB', 'MAC', 'M', 'MNK', 'MRO', 'MPC', 'MAR', 'MMC', 'MLM', 'MAS', 'MA', 'MAT', 'MKC', 'MCD',
+		    'MHFI', 'MCK', 'MJN', 'MMV', 'MDT', 'MRK', 'MET', 'KORS', 'MCHP', 'MU', 'MSFT', 'MHK', 'TAP', 'MDLZ', 'MON', 'MNST', 'MCO', 'MS', 'MOS', 'MSI',
+		    'MUR', 'MYL', 'NDAQ', 'NOV', 'NAVI', 'NTAP', 'NFLX', 'NWL', 'NFX', 'NEM', 'NWSA', 'NEE', 'NLSN', 'NKE', 'NI', 'NE', 'NBL', 'JWN', 'NSC', 'NTRS',
+		    'NOC', 'NRG', 'NUE', 'NVDA', 'ORLY', 'OXY', 'OMC', 'OKE', 'ORCL', 'OI', 'PCAR', 'PLL', 'PH', 'PDCO', 'PAYX', 'PNR', 'PBCT', 'POM', 'PEP', 'PKI',
+		    'PRGO', 'PFE', 'PCG', 'PM', 'PSX', 'PNW', 'PXD', 'PBI', 'PCL', 'PNC', 'RL', 'PPG', 'PPL', 'PX', 'PCP', 'PCLN', 'PFG', 'PG', 'PGR', 'PLD', 'PRU',
+		    'PEG', 'PSA', 'PHM', 'PVH', 'QRVO', 'PWR', 'QCOM', 'DGX', 'RRC', 'RTN', 'O', 'RHT', 'REGN', 'RF', 'RSG', 'RAI', 'RHI', 'ROK', 'COL', 'ROP', 'ROST',
+		    'RLC', 'R', 'CRM', 'SNDK', 'SCG', 'SLB', 'SNI', 'STX', 'SEE', 'SRE', 'SHW', 'SIAL', 'SPG', 'SWKS', 'SLG', 'SJM', 'SNA', 'SO', 'LUV', 'SWN', 'SE',
+		    'STJ', 'SWK', 'SPLS', 'SBUX', 'HOT', 'STT', 'SRCL', 'SYK', 'STI', 'SYMC', 'SYY', 'TROW', 'TGT', 'TEL', 'TE', 'TGNA', 'THC', 'TDC', 'TSO', 'TXN',
+		    'TXT', 'HSY', 'TRV', 'TMO', 'TIF', 'TWX', 'TWC', 'TJK', 'TMK', 'TSS', 'TSCO', 'RIG', 'TRIP', 'FOXA', 'TSN', 'TYC', 'UA', 'UNP', 'UNH', 'UPS', 'URI',
+		    'UTX', 'UHS', 'UNM', 'URBN', 'VFC', 'VLO', 'VAR', 'VTR', 'VRSN', 'VZ', 'VRTX', 'VIAB', 'V', 'VNO', 'VMC', 'WMT', 'WBA', 'DIS', 'WM', 'WAT', 'ANTM',
+		    'WFC', 'WDC', 'WU', 'WY', 'WHR', 'WFM', 'WMB', 'WEC', 'WYN', 'WYNN', 'XEL', 'XRX', 'XLNX', 'XL', 'XYL', 'YHOO', 'YUM', 'ZBH', 'ZION', 'ZTS'];
+
+const express = require('express');
+const router = express.Router();
+const Player = require('../models/player');
+const League = require('../models/league');
+var request = require('request');
+//var limit = require("simple-rate-limiter");
+//var request = limit(require("request")).to(1).per(5000);
 
 
-    // get and list players from database
-    router.get('/players', function(req, res, next){
-        Player.find({}).then(function(players){
-            res.send(players);
-        })
-    });
+
+// get and list players from database
+router.get('/players', function(req, res, next){
+   Player.find({}).then(function(players){
+      res.send(players);
+   })
+});
 
     // add a new player to database
-    router.post('/players', function(req, res, next){
-        Player.create(req.body).then(function(player){
-            res.send(player);
-        }).catch(next);
-    });
+router.post('/players', function(req, res, next){
+   Player.create(req.body).then(function(player){
+       res.send(player);
+   }).catch(next);
+});
 
-    // update a player in database
-    router.put('/players/:id', function(req, res, next){
-         Player.findByIdAndUpdate({_id: req.params.id}, req.body).then(function(player){
-            Player.findOne({_id: req.params.id}).then(function(player){
-                res.send(player);
-            }); 
-         });
-    });
+// update a player in database
+router.put('/players/:id', function(req, res, next){
+   Player.findByIdAndUpdate({_id: req.params.id}, req.body).then(function(player){
+      Player.findOne({_id: req.params.id}).then(function(player){
+         res.send(player);
+     }); 
+   });
+});
 
-    // delete a player from database
-    router.delete('/players/:id', function(req, res, next){
-        Player.findByIdAndRemove({_id: req.params.id}).then(function(player){
-            res.send(player);
-        })
-    });
+// delete a player from database
+router.delete('/players/:id', function(req, res, next){
+  Player.findByIdAndRemove({_id: req.params.id}).then(function(player){
+       res.send(player);
+  })
+});
 
 router.get('/leagues', function(req, res, next){
    League.find({}).then(function(leagues){
@@ -41,19 +70,44 @@ router.get('/leagues', function(req, res, next){
    })
 });
 
-router.get('/stock/:key', function(req,res,next){
+router.get('/stock', function(req,res,next){
+   
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");   
+    
+    var stockObj = [];
+    var stocks = [];
+    numStocks = 4;
+    completed_requests = 0;
+    for (i = 0; i < numStocks; i++){
        
-    var request = require('request');
-    var key = req.params.key;
-    request({url:'http://dev.markitondemand.com/Api/v2/Quote/json',qs: {symbol : key}},function(error,response,body){
-       if (!error && response.statusCode == 200){
-           var stockObj = JSON.parse(body);
-           res.json({ Name : stockObj.Name, Price : stockObj.LastPrice});    
-        	
-       }
+        request({url:'http://dev.markitondemand.com/Api/v2/Quote/json',qs: {symbol : stockSymbols[i]}},function(error,response,body){
+           
+           if (!error && response.statusCode == 200){
+               stockJSON = JSON.parse(body);
+	       //console.log(1);
+	       stockObj.push({'Name' : stockJSON.Name, 'Price' : stockJSON.LastPrice});
+	       completed_requests++;
+	       //console.log(stockSymbols[i]);
+	       
+	       if (completed_requests == numStocks){
+                   //console.log(stockObj);
+                   res.json(JSON.stringify(stockObj));   
+	       }
+                
+                
+	   }
+	    else { console.log(body); } 	   
+		
+	});
+ 
+	         
 	    
-    });
+   
+
+    }	  
+     
 });
 
-    module.exports = router;
+module.exports = router;
 
