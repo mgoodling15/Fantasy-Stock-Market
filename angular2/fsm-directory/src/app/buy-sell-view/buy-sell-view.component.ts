96a10f6fa470838d1b3dddceee8bc0dc249bfc11
alherronWC <alexanderfherron@gmail.com>
2017-04-19 14:27:42 -0400

    Add files via upload

--- angular2/fsm-directory/src/app/buy-sell-view/buy-sell-view.component.ts
+++ angular2/fsm-directory/src/app/buy-sell-view/buy-sell-view.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Http, Response } from '@angular/http';
+//import { UserService } from '../shared/user.service';
 
 @Component({
   selector: 'app-buy-sell-view',
@@ -8,29 +10,33 @@ import { ActivatedRoute } from '@angular/router';
   providers: []
 })
 export class BuySellViewComponent implements OnInit {
-
-  stocks = [
-    {
-      "name": "Amazon",
-      "value": 123456
-    },
-    {
-      "name": "Microsoft",
-      "value": 747383
-    },
-    {
-      "name": "Exxon",
-      "value": 970442
-    },
-    {
-      "name": "Panera",
-      "value": 2554373
-    }
-  ]
-
-  constructor() { }
+ public stockLst;
+ constructor(private http: Http) {}
+ 
+  stocks = [];
 
   ngOnInit() {
+     
+     this.getStocks();
+     
+     //this.fillStocks();
+  }
+
+  getStocks() {
+    this.http.get('http://localhost:4000/api/stock')
+      .map((res:Response) => res.json())
+      .subscribe(
+        data => { this.fillStocks(JSON.parse(data));},
+        err => console.error(err),
+        () => console.log('done')
+      );
+     
+     
+  }
+  fillStocks(data){
+   for (var i = 0; i < 4; i++){
+       this.stocks.push({"name": data[i].Name, "value": data[i].Price}); 
+   }
   }
 
 }
