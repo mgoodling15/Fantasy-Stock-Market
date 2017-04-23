f34a6e7a46330fac8d6a21c568bf35874d90ebf7
mgoodling15 <megoodling@davidson.edu>
2017-04-23 19:51:04 -0400

    Update main.ts

--- angular2/fsm-directory/src/main.ts
+++ angular2/fsm-directory/src/main.ts
@@ -1,11 +1,16 @@
+//file that sets up modules for the app
+
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+//import { Http, HttpModule, RequestOptions } from '@angular/http';
 
 if (environment.production) {
   enableProdMode();
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule);
+//platformBrowserDynamic().bootstrapModule(HttpModule);
+//platformBrowserDynamic().bootstrapModule(Http);
+//platformBrowserDynamic().bootstrapModule(RequestOptions);
