e80e6a13e5d18efb1cf4362bc4b7e348817b9fed
mgoodling15 <megoodling@davidson.edu>
2017-04-23 20:43:07 -0400

    Update login-form.component.ts

--- angular2/fsm-directory/src/app/login-form/login-form.component.ts
+++ angular2/fsm-directory/src/app/login-form/login-form.component.ts
@@ -1,16 +1,34 @@
+//file for implementation of login form component 
+
 import { Component } from '@angular/core';
 import { Player }    from '../player';
+import { DataService } from '../data.service';
+declare var firebase: any;
 
 @Component({
   selector: 'login-form',
-  templateUrl: './login-form.component.html'
+  templateUrl: './login-form.component.html',
+  providers: [DataService]
 })
 export class LoginFormComponent {
-
-  model = new Player("Email", "Username", "Password", "Bio");
-
+  errorMessage: string;
+  mode = 'Observable';
   submitted = false;
+  model = new Player("Email", "Username", "Password", "Bio");
+  loggedIn = false;
+
+  constructor (private dataService: DataService) {}
+  ngOnInit() { }
 
   onSubmit() { this.submitted = true; }
 
+  fbSignIn(email, password){
+    firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+
+    });
+
+  }
 }
