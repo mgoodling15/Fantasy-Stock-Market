b11bf8e57bceefcc24f41c3189b5ed67b9ffc5b3
mgoodling15 <megoodling@davidson.edu>
2017-04-23 20:46:52 -0400

    Update player-view.component.ts

--- angular2/fsm-directory/src/app/player-view/player-view.component.ts
+++ angular2/fsm-directory/src/app/player-view/player-view.component.ts
@@ -1,6 +1,10 @@
+//file for implementation of player view component 
+
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { DataService } from '../data.service';
+import { LoginFormComponent } from '../login-form/login-form.component';
+declare var firebase: any;
 
 @Component({
   selector: 'app-player-view',
@@ -9,7 +13,7 @@ import { DataService } from '../data.service';
   providers: [DataService]
 })
 export class PlayerViewComponent implements OnInit {
-  classes = {'blue': false, 'red': true, 'underline': false};
+  //classes = {'blue': false, 'red': true, 'underline': false};
   player: string;
 
   constructor(private route: ActivatedRoute, private dataService: DataService) {
@@ -19,6 +23,19 @@ export class PlayerViewComponent implements OnInit {
    players = []
 
   ngOnInit() {
+    var username, email, uid;
+
+    var user = firebase.auth().currentUser;
+
+    if (user != null) {
+      uid = user.uid;
+    }
+
+  firebase.database().ref('/players/' + uid).once('value').then(function(snapshot) {
+  //var username = snapshot.val().username;
+  //console.log(snapshot);
+
+});
 
   }
 
