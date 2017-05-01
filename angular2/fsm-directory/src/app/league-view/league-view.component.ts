e564e024c4da8b94e583e4891b4469bea84675fe
mgoodling15 <megoodling@davidson.edu>
2017-05-01 16:31:42 -0400

    Update league-view.component.ts

--- angular2/fsm-directory/src/app/league-view/league-view.component.ts
+++ angular2/fsm-directory/src/app/league-view/league-view.component.ts
@@ -1,8 +1,10 @@
-//file for implementation of league view page component 
+//file for implementation of league view page component
 
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { DataService } from '../data.service';
+import { LeagueFormComponent } from '../league-form/league-form.component';
+import { League } from '../league';
 declare var firebase: any;
 
 @Component({
@@ -14,6 +16,7 @@ declare var firebase: any;
 export class LeagueViewComponent implements OnInit {
   league: string;
   player: string;
+  model = new League("League Name");
 
   players = []
 
@@ -22,14 +25,62 @@ export class LeagueViewComponent implements OnInit {
    }
 
    ngOnInit() {
-      this.fbGetData();
+      //this.fbGetData();
    }
 
-   fbGetData() {
-    firebase.database().ref('/players').on('child_added', (snapshot) => {
-      this.players.push(snapshot.val());
-      //list players in db by id number, not username
-    })
+  //  fbGetData() {
+  //   firebase.database().ref('/players').on('child_added', (snapshot) => {
+  //     this.players.push(snapshot.val());
+  //     //list players in db by id number, not username
+  //   })
 
+  //}
+
+  joinLeague(leaguename){
+    var user = firebase.auth().currentUser;
+    var uid = user.uid;
+
+    //find count or set to zero if doesnt exist
+    firebase.database().ref('leagues/' + leaguename).once('value', function(snapshot) {
+      var count = snapshot.child('count').val();
+      if (count == null){
+        count = 0;
+        firebase.database().ref('leagues/' + leaguename + '/count/').set(count
+        )
+      }
+
+      //check if player exists in league
+      var exists = false;
+      firebase.database().ref('leagues/' + leaguename + '/players/').once('value', function(snapshot){
+        snapshot.forEach(function(child){
+          if (child.val() == uid){
+            exists = true;
+            console.log("user already in league");
+          }
+        });
+        if (exists == true){
+          alert("You have already joined this league!");
+        }
+
+        //check if within amount of players allowed
+        if ((count < 16) && (count >= 0) && (!(exists))){ //hard code 16
+          count++;
+          firebase.database().ref('leagues/' + leaguename + '/count/').set(count
+          ).then(
+          firebase.database().ref('leagues/' + leaguename).once('value', function(snapshot){
+              firebase.database().ref('leagues/' + leaguename + '/players/' + count).set(uid
+                );
+            }).then(
+              firebase.database().ref('players/' + uid + '/league/').set(leaguename)
+            )
+          );
+
+        }if (count == 16){
+          alert("There are already 16 players in this league - join another!");
+          console.log("too many league members");
+        }
+    });
+  });
   }
+
 }
