9fea34cbb7eb4b6f3fda9e5d0409e63ef9ac3280
mgoodling15 <megoodling@davidson.edu>
2017-05-01 16:32:26 -0400

    Update login-form.component.ts

--- angular2/fsm-directory/src/app/login-form/login-form.component.ts
+++ angular2/fsm-directory/src/app/login-form/login-form.component.ts
@@ -1,9 +1,10 @@
-//file for implementation of login form component 
+//file for implementation of login form component
 
 import { Component } from '@angular/core';
 import { Player }    from '../player';
 import { DataService } from '../data.service';
 declare var firebase: any;
+import { PlayerFormComponent } from '../player-form/player-form.component';
 
 @Component({
   selector: 'login-form',
@@ -16,19 +17,63 @@ export class LoginFormComponent {
   submitted = false;
   model = new Player("Email", "Username", "Password", "Bio");
   loggedIn = false;
+  isSignedIn = false;
+  submitted1 = false;
+  model1 = new Player("Email", "Username", "Password", "Bio");
 
   constructor (private dataService: DataService) {}
   ngOnInit() { }
 
-  onSubmit() { this.submitted = true; }
+  onSubmit() {
+    this.submitted = true;
+    var user = firebase.auth().currentUser;
+    if (user != null){
+      this.isSignedIn = true;
+    }
+  }
+
+  onSubmit1() {
+    this.submitted1 = true;
+    var user = firebase.auth().currentUser;
+    if (user != null){
+      this.isSignedIn = true;
+    }
+  }
 
   fbSignIn(email, password){
     firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
       // Handle Errors here.
+      if(error){
+        alert("Invalid username or passord.");
+        var errorCode = error.code;
+        var errorMessage = error.message;
+      }
+    });
+  }
+
+  fbCreatePlayer(email, username, password, bio){
+    return firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+    if (error) {
+      alert("Invalid info for create account.")
+      console.log('Login Failed!', error);
+    } else {
+      this.isSignedIn = true;
+      console.log('Authenticated successfully');
+    }
       var errorCode = error.code;
       var errorMessage = error.message;
-
-    });
-
+    })
   }
+
+  fbPostPlayer(email, username, password, bio){
+    this.fbCreatePlayer(email, username, password, bio).then(function(value){
+      firebase.auth().signInWithEmailAndPassword(email, password);
+      var user = firebase.auth().currentUser;
+      var uid = user.uid;
+      firebase.database().ref('players/' + uid).set({username: username, email: email,
+        password: password, bio: bio
+      });
+    });
+  }
+
 }
