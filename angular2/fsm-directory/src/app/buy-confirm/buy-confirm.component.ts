70f32372d645e5cb8b2214494fd711e051694cc2
alherronWC <alexanderfherron@gmail.com>
2017-05-09 15:23:52 -0400

    Add files via upload

--- angular2/fsm-directory/src/app/buy-confirm/buy-confirm.component.ts
+++ angular2/fsm-directory/src/app/buy-confirm/buy-confirm.component.ts
@@ -1,17 +1,29 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-buy-confirm',
   templateUrl: './buy-confirm.component.html',
   styleUrls: ['./buy-confirm.component.css']
 })
+
+
+
 export class BuyConfirmComponent implements OnInit, OnDestroy {
   public name;
   public value;
   private sub: any;
-  
-  constructor(private route : ActivatedRoute) { }
+  public purchase = this.fb.group({
+    quantity: ["",Validators.required]
+  });
+
+  buy() {
+     //console.log(event);
+     console.log(this.purchase.value.quantity);
+  }
+
+  constructor(private route : ActivatedRoute, public fb: FormBuilder) { }
 
   ngOnInit() {
      this.sub = this.route.queryParams.subscribe(params => {
