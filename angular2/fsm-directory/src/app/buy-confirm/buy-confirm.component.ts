263df037ac8c76236e1d14641147f788b28b8b7d
alherronWC <alexanderfherron@gmail.com>
2017-05-10 00:28:47 -0400

    Add files via upload

--- angular2/fsm-directory/src/app/buy-confirm/buy-confirm.component.ts
+++ angular2/fsm-directory/src/app/buy-confirm/buy-confirm.component.ts
@@ -14,38 +14,60 @@ declare var firebase: any;
 export class BuyConfirmComponent implements OnInit, OnDestroy {
   public name;
   public value;
+  public cash: number;
+  public newCash: number;
   private sub: any;
+  public uid;
+  public cost;
+  public quan;
+  public state = 'input';
   public purchase = this.fb.group({
     quantity: ["",Validators.required]
   });
 
   buy() {
      //console.log(event);
-     var quan = this.purchase.value.quantity;
-     var user = firebase.auth().currentUser;
-     var uid = user.uid;
+     this.quan = this.purchase.value.quantity;
+    
      var currentQuan;
-     var ref = firebase.database().ref('players/' + uid + '/stocks/' +  this.name + '/');
+     var ref = firebase.database().ref('players/' + this.uid + '/stocks/' +  this.name + '/');
      ref.on("value", (snapshot) => {
         currentQuan = snapshot.val();
      });
      if (currentQuan == null){
         currentQuan = 0;
      }
-
-     firebase.database().ref('players/' + uid + '/stocks/' +  this.name + '/').set(parseInt(quan) + parseInt(currentQuan));
-     //firebase.database().ref('players/' + uid + '/stock/').set(quan);
+     this.cost = Math.round(Number(this.quan * this.value) * 100) / 100;
+    
+     if (Number(this.cost) < Number(this.cash)) {
+         firebase.database().ref('players/' + this.uid + '/stocks/' +  this.name + '/').set(parseInt(this.quan) + parseInt(currentQuan));
+	 var costRef = firebase.database().ref('players/' + this.uid + '/cash/');
+	 costRef.set(Number(this.cash) - Number(this.cost));
+	 costRef.on('value',(snapshot) => {
+            this.newCash = snapshot.val();
+	 });
+     
+	 this.state = 'confirm';
+     }
+     else {
+        
+        this.state = 'deny';
+     }
   }
 
   constructor(private route : ActivatedRoute, public fb: FormBuilder) { }
 
   ngOnInit() {
-    var user = firebase.auth().currentUser;
-    var uid = user.uid;
      this.sub = this.route.queryParams.subscribe(params => {
-
+            
             this.name = params['name'];
             this.value = +params['value'];
+     });
+     var user = firebase.auth().currentUser;
+     var uid = user.uid;
+     var costRef = firebase.database().ref('players/' + uid + '/cash/');
+     costRef.on('value',(snapshot) => {
+        this.cash = snapshot.val();
      });
   }
   ngOnDestroy(){
