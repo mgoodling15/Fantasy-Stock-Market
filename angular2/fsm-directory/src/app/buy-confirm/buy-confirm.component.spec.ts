cb330d15a360ba19685f1834055c5e1c3efb2f44
mgoodling15 <megoodling@davidson.edu>
2017-05-09 23:56:18 -0400

    Update buy-confirm.component.spec.ts

--- angular2/fsm-directory/src/app/buy-confirm/buy-confirm.component.spec.ts
+++ angular2/fsm-directory/src/app/buy-confirm/buy-confirm.component.spec.ts
@@ -1,25 +1,55 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { FormBuilder, Validators } from '@angular/forms';
+declare var firebase: any;
 
-import { BuyConfirmComponent } from './buy-confirm.component';
+@Component({
+  selector: 'app-buy-confirm',
+  templateUrl: './buy-confirm.component.html',
+  styleUrls: ['./buy-confirm.component.css']
+})
 
-describe('BuyConfirmComponent', () => {
-  let component: BuyConfirmComponent;
-  let fixture: ComponentFixture<BuyConfirmComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ BuyConfirmComponent ]
-    })
-    .compileComponents();
-  }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(BuyConfirmComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+export class BuyConfirmComponent implements OnInit, OnDestroy {
+  public name;
+  public value;
+  private sub: any;
+  public purchase = this.fb.group({
+    quantity: ["",Validators.required]
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+  buy() {
+     //console.log(event);
+     var quan = this.purchase.value.quantity;
+     var user = firebase.auth().currentUser;
+     var uid = user.uid;
+     var currentQuan;
+     var ref = firebase.database().ref('players/' + uid + '/stocks/' +  this.name + '/');
+     ref.on("value", (snapshot) => {
+        currentQuan = snapshot.val();
+     });
+     if (currentQuan == null){
+        currentQuan = 0;
+     }
+
+     firebase.database().ref('players/' + uid + '/stocks/' +  this.name + '/').set(parseInt(quan) + parseInt(currentQuan));
+     //firebase.database().ref('players/' + uid + '/stock/').set(quan);
+  }
+
+  constructor(private route : ActivatedRoute, public fb: FormBuilder) { }
+
+  ngOnInit() {
+    var user = firebase.auth().currentUser;
+    var uid = user.uid;
+     this.sub = this.route.queryParams.subscribe(params => {
+
+            this.name = params['name'];
+            this.value = +params['value'];
+     });
+  }
+  ngOnDestroy(){
+    this.sub.unsubscribe();
+  }
+
+}
